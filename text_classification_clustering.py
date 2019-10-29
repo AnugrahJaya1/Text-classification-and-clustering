2af4a3019924e93d6cabd6f1efcc9d624fe6e2e2
Anugrah Jaya <anugrahjaya1.bts.internship@gmail.com>
2019-10-29 11:22:23 +0700

    mengelompokkan menjadi clustering dan classification

--- text_classification_clustering.py
+++ text_classification_clustering.py
@@ -7,8 +7,6 @@ Created on Mon Jan  8 14:36:07 2018
 
 # Importing libraries
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.cluster import KMeans
 from nltk.corpus import stopwords 
 from nltk.stem.wordnet import WordNetLemmatizer
 import string
@@ -16,13 +14,13 @@ import re
 import numpy as np
 from collections import Counter
 
-from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import KFold
 from sklearn import metrics
 
-import nltk
-nltk.download('stopwords')
-nltk.download('wordnet')
+#import nltk
+#nltk.download('stopwords')
+#nltk.download('wordnet')
+
 
 stop = set(stopwords.words('english'))
 exclude = set(string.punctuation) 
@@ -38,7 +36,7 @@ def clean(doc):
     return y
 
 
-print ("There are 10 sentences of following three classes on which K-NN classification and K-means clustering"\
+print ("\nThere are 10 sentences of following three classes on which K-NN classification and K-means clustering"\
          " is performed : \n1. Cricket \n2. Artificial Intelligence \n3. Chemistry")
 path = "Sentences.txt"
 
@@ -51,47 +49,54 @@ for line in fp:
     train_clean_sentences.append(cleaned)
        
 vectorizer = TfidfVectorizer(stop_words='english')
+# data x
 X = vectorizer.fit_transform(train_clean_sentences)
 
 # Creating true labels for 30 training sentences 
+# data y
 y_train = np.zeros(30)
 y_train[10:20] = 1
 y_train[20:30] = 2
 
 
-# Clustering the document with KNN classifier
-modelknn = KNeighborsClassifier(n_neighbors=5)
-modelknn.fit(X,y_train)
+true_test_labels = ['Cricket','AI','Chemistry']
 
-# Clustering the training 30 sentences with K-means technique
-modelkmeans = KMeans(n_clusters=3, init='k-means++', max_iter=200, n_init=100)
-modelkmeans.fit(X)
-
+#Data set untuk test
 test_sentences = ["Chemical compunds are used for preparing bombs based on some reactions",\
                   "Cricket is a boring game where the batsman only enjoys the game",\
                   "Machine learning is an area of Artificial intelligence"]
 
+# membersihkan data
 test_clean_sentence = []
 for test in test_sentences:
     cleaned_test = clean(test)
     cleaned = ' '.join(cleaned_test)
     cleaned = re.sub(r"\d+","",cleaned)
     test_clean_sentence.append(cleaned)
-    
+
+# Data set yang siap digunakan untuk test
 Test = vectorizer.transform(test_clean_sentence) 
 
-true_test_labels = ['Cricket','AI','Chemistry']
-predicted_labels_knn = modelknn.predict(Test)
-predicted_labels_kmeans = modelkmeans.predict(Test)
-
+#print data set untuk test
 print ("\nBelow 3 sentences will be predicted against the learned nieghbourhood and learned clusters:\n1. ",\
         test_sentences[0],"\n2. ",test_sentences[1],"\n3. ",test_sentences[2])
-print ("\n-------------------------------PREDICTIONS BY KNN------------------------------------------")
+
+print("\n-------------------PREDICTION USING CLASSIFICATION-------------------")
+
+#Classification
+
+#KNN
+from sklearn.neighbors import KNeighborsClassifier
+modelknn = KNeighborsClassifier(n_neighbors=5)
+modelknn.fit(X,y_train)
+predicted_labels_knn = modelknn.predict(Test)
+
+print ("\n-------------------------PREDICTIONS BY KNN-------------------------")
 print ("\n",test_sentences[0],":",true_test_labels[np.int(predicted_labels_knn[0])],\
         "\n",test_sentences[1],":",true_test_labels[np.int(predicted_labels_knn[1])],\
-        "\n",test_sentences[2],":",true_test_labels[np.int(predicted_labels_knn[2])])
+        "\n",test_sentences[2],":",true_test_labels[np.int(predicted_labels_knn[2])],"\n")
 
-
+# Data untuk split ke dalam KFold
 X = X.toarray()
 Y = y_train
 
@@ -103,34 +108,20 @@ for train_id, test_id in kf.split(X):
     modelknn.fit(x_train, y_train);
     y_pred = modelknn.predict(x_test)
     acc.append(metrics.accuracy_score(y_test, y_pred))
-    
-print("Akurasi dengan knn = ",np.mean(acc),"\n")
 
+print("Akurasi dengan knn","dengan k =",5," = ",np.mean(acc),"\n")
 
-print ("\n-------------------------------PREDICTIONS BY K-Means--------------------------------------")
-print ("\nIndex of Cricket cluster : ",Counter(modelkmeans.labels_[0:10]).most_common(1)[0][0])
-print ("Index of Artificial Intelligence cluster : ",Counter(modelkmeans.labels_[10:20]).most_common(1)[0][0]) 
-print ("Index of Chemistry cluster : ",Counter(modelkmeans.labels_[20:30]).most_common(1)[0][0])
-
-print ("\n",test_sentences[0],":",true_test_labels[np.int(predicted_labels_knn[0])],\
-        "\n",test_sentences[1],":",true_test_labels[np.int(predicted_labels_knn[1])],\
-        "\n",test_sentences[2],":",true_test_labels[np.int(predicted_labels_knn[2])])
-
-
-#print ("\n",test_sentences[0],":",predicted_labels_kmeans[0],\
-#        "\n",test_sentences[1],":",predicted_labels_kmeans[1],\
-#        "\n",test_sentences[2],":",predicted_labels_kmeans[2])
-
-
+#Naive Bayes
+from sklearn.naive_bayes import GaussianNB
 nb = GaussianNB()
 
 nb.fit(X,Y)
 predicted_labels_nb = nb.predict(Test.toarray())
 
-print ("\n-------------------------------PREDICTIONS BY Naive Bayes------------------------------------------")
+print ("\n-------------------------PREDICTIONS BY NAIVE BAYES-------------------------")
 print ("\n",test_sentences[0],":",true_test_labels[np.int(predicted_labels_nb[0])],\
         "\n",test_sentences[1],":",true_test_labels[np.int(predicted_labels_nb[1])],\
-        "\n",test_sentences[2],":",true_test_labels[np.int(predicted_labels_nb[2])])
+        "\n",test_sentences[2],":",true_test_labels[np.int(predicted_labels_nb[2])],"\n")
 
 acc=[]
 for train_id, test_id in kf.split(X):
@@ -142,13 +133,14 @@ for train_id, test_id in kf.split(X):
     
 print("Akurasi dengan naive bayes = ",np.mean(acc),"\n")
 
+#Decision Tree
 from sklearn.tree import DecisionTreeClassifier
 tree = DecisionTreeClassifier()
 
 tree.fit(X,Y)
 predicted_labels_tree=tree.predict(Test)
 
-print ("\n-------------------------------PREDICTIONS BY Decesion Tree------------------------------------------")
+print ("\n-------------------------PREDICTIONS BY Decesion Tree-------------------------")
 print ("\n",test_sentences[0],":",true_test_labels[np.int(predicted_labels_tree[0])],\
         "\n",test_sentences[1],":",true_test_labels[np.int(predicted_labels_tree[1])],\
         "\n",test_sentences[2],":",true_test_labels[np.int(predicted_labels_tree[2])])
@@ -162,3 +154,31 @@ for train_id, test_id in kf.split(X):
     acc.append(metrics.accuracy_score(y_test, y_pred))
     
 print("Akurasi dengan decision tree = ",np.mean(acc),"\n")
+
+
+# Clustering the training 30 sentences with K-means technique
+from sklearn.cluster import KMeans
+modelkmeans = KMeans(n_clusters=3, init='k-means++', max_iter=200, n_init=100)
+modelkmeans.fit(X)
+predicted_labels_kmeans = modelkmeans.predict(Test)
+
+print ("\n-------------------------PREDICTIONS BY K-Means-------------------------")
+print ("\nIndex of Cricket cluster : ",Counter(modelkmeans.labels_[0:10]).most_common(1)[0][0])
+print ("Index of Artificial Intelligence cluster : ",Counter(modelkmeans.labels_[10:20]).most_common(1)[0][0]) 
+print ("Index of Chemistry cluster : ",Counter(modelkmeans.labels_[20:30]).most_common(1)[0][0])
+
+print ("\n",test_sentences[0],":",true_test_labels[np.int(predicted_labels_kmeans[0])],\
+        "\n",test_sentences[1],":",true_test_labels[np.int(predicted_labels_kmeans[1])],\
+        "\n",test_sentences[2],":",true_test_labels[np.int(predicted_labels_kmeans[2])],"\n")
+
+acc = []
+dist = []
+for train_id, test_id in kf.split(X):
+    x_train, x_test = X[train_id], X[test_id]
+    y_train, y_test = Y[train_id], Y[test_id]
+    modelkmeans.fit(x_train);
+    y_pred = modelkmeans.predict(x_test)
+    acc.append(metrics.accuracy_score(y_test, y_pred))
+    
+print("Akurasi dengan K-Means = ",np.mean(acc),"\n")
+
